d6d69b5e38c6c6821c26d18a460cf64155d46664
Douglas Masho <douglasmasho@gmail.com>
2021-10-06 12:36:26 +0200

    Add project files.

---
+++ SquareRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sqrt = 0;
+            Console.WriteLine("Please enter a number to be square rooted");
+
+            try
+            {
+                double inpNum = Convert.ToDouble(Console.ReadLine());
+                if(inpNum < 0)
+                {
+                    throw new InvalidOperationException("Cannot Square root a negative.");
+                }
+                sqrt = Math.Sqrt(inpNum);
+            }catch(Exception e)
+            {
+                sqrt = 0;
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine($"The square root is {sqrt}");
+            }
+        }
+    }
+}
